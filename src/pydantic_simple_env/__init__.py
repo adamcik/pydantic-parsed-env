f0bffcc4d53205a47604b52e3329a9383034a54e
Thomas Adamcik <thomas@adamcik.no>
2026-03-29 20:27:32 +0200

    build: derive package version from VCS and expose __version__

--- src/pydantic_simple_env/__init__.py
+++ src/pydantic_simple_env/__init__.py
@@ -5,6 +5,8 @@ dictionaries) from environment variables using simple, delimited strings
 instead of complex JSON.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from ._api import (
     BaseSimpleEnvSettings,
     SimpleEnvConfig,
@@ -13,10 +15,16 @@ from ._api import (
     SimpleParsed,
 )
 
+try:
+    __version__ = version("pydantic-simple-env")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
 __all__ = [
     "BaseSimpleEnvSettings",
     "SimpleEnvConfig",
     "SimpleEnvParser",
     "SimpleEnvSettingsSource",
     "SimpleParsed",
+    "__version__",
 ]
